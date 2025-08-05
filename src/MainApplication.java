ad0ce1c33dc8d8c2acb7756e43c6c581fce01aba
vera <HighSkillGirl@yandex.com>
2025-08-05 22:37:00 +0300

    refactoring + marker some dates

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,27 +1,35 @@
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class MainApplication {
-    public static void main(String[] args) {
+    public static void main(String[] programmingDays) {
+
         LocalDate today = LocalDate.of(2025, 8, 1);
         int dayOfWeek =  today.getDayOfWeek().getValue();
+        StringBuilder calendar = getCalendar(today, dayOfWeek);
+
+        Arrays.stream(programmingDays)
+              .forEach(day -> {
+                  int dayStartIndex = calendar.indexOf(day) + day.length();
+                  calendar.replace(dayStartIndex, dayStartIndex + 1, "*");
+              });
+
+        System.out.print(calendar);
+    }
+
+    private static StringBuilder getCalendar(LocalDate today, int dayOfWeek) {
         int monthSize = today.lengthOfMonth();
 
-        System.out.println("ПН  ВТ  СР  ЧТ  ПТ  СБ  ВС");
-
-        StringBuilder emptyLine = new StringBuilder();
-        for (int i = 0; i < ( dayOfWeek - 1); i++) {
-            emptyLine.append("    ");
-        }
-
-        System.out.print(emptyLine);
+        StringBuilder calendar = new StringBuilder("ПН  ВТ  СР  ЧТ  ПТ  СБ  ВС\n");
+        calendar.append("    ".repeat(Math.max(0, (dayOfWeek - 1))));
 
         for (int i = 0, dayOfMonth = today.getDayOfMonth(); dayOfMonth <= monthSize; i++) {
             for (int j = (i == 0 ? dayOfWeek - 1 : 0); j < 7 && dayOfMonth <= monthSize; j++) {
-                System.out.printf("%2d  ", dayOfMonth);
+                calendar.append(String.format("%2d  ", dayOfMonth));
                 dayOfMonth++;
             }
-            System.out.println();
+            calendar.append("\n");
         }
-
+        return calendar;
     }
 }
